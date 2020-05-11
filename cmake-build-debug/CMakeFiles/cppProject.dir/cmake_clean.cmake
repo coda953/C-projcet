fefc74f89d6164051c0010dfad98afd1209b28f8
coda953 <11712504@mail.sustc.edu.cn>
2020-05-11 22:04:01 +0800

    5.11 add

---
+++ cmake-build-debug/CMakeFiles/cppProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppProject.dir/Matrix.cpp.o"
+  "CMakeFiles/cppProject.dir/main.cpp.o"
+  "cppProject.exe"
+  "cppProject.pdb"
+  "libcppProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
