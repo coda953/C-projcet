d5bcd9b48895c185f74c80e96169cdd2c1c3a43e
zhujunda <11712504@mail.sustc.edu.cn>
2020-06-13 22:48:04 +0800

    这就是矩阵相加的力量

--- code/main.cpp
+++ code/main.cpp
@@ -6,13 +6,18 @@
 using namespace std;
 using namespace cv;
 const char *tarMatrix = "img/SunShangXiang.jpeg";
+const char *test1 = "img/test1_1.png";
+const char *test2 = "img/test1_2.png";
 
 int main() {
-    Mat srcImage = imread(tarMatrix, IMREAD_GRAYSCALE);
+    Mat Imagetest1 = imread(test1, IMREAD_GRAYSCALE);
+    Mat Imagetest2 = imread(test2, IMREAD_GRAYSCALE);
     //change to our matrix
-    Matrix<uchar> ourMatrix = srcImage;
+    Matrix<uchar> m1 = Imagetest1;
+    Matrix<uchar> m2 = Imagetest2;
+    Matrix<uchar> result = m1 + m2;
     //change to opencv mat
-    Mat outImage = ourMatrix.to_opencv_8UC1();
+    Mat outImage = result.to_opencv_8UC1();
     imshow("[img]", outImage);
     waitKey(0);
     return 0;
